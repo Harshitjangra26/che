c22e64fe4accb7a6b10acad440b8596fddd41b2f
Harshitjangra26 <harshitjangra2006@gmail.com>
2024-10-26 23:32:17 +0530

    che

---
+++ che.js
@@ -0,0 +1,5 @@
+// JavaScript functionality can be added here
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Document is ready.");
+    // Additional functionality can be implemented
+});
